cf7abba0c0a6b96953e750a61b3e13d860e678ea
Dhruba Baishya <dhruvajyoti26@gmail.com>
2026-05-01 10:46:52 +0530

    Updated Google Maps embed and WhatsApp button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,11 +30,11 @@ function Contact() {
             <iframe
               title="Keshab Room Rent Location"
               width="100%"
-              height="300"
+              height="400"
               style={{ border: 0, borderRadius: '4px' }}
               loading="lazy"
               allowFullScreen
-              src="https://maps.google.com/maps?q=26.190332,91.713976&z=17&output=embed"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3580.177768452455!2d91.71740350385994!3d26.1908948735814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375a5baebd7b0f13%3A0x5819dbe3dcc240b2!2sKeshab(Room%20Rent)!5e0!3m2!1sen!2sin!4v1777611835708!5m2!1sen!2sin"
             />
           </div>
         )}
